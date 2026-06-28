f331b86a9ca83c6b42e884cb6bec8c9f9814dfab
PeterWuCs <157049093+PeterWuCs@users.noreply.github.com>
2026-06-27 22:34:45 -0400

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        distance = [-1] * n
+        distance[k - 1] = 0
+
+        pq = [(0, k)]
+
+        g = defaultdict(list)
+        relaxed = set()
+
+        for i in range(len(times)):
+            g[times[i][0]].append((times[i][2], times[i][1]))
+        
+        while pq and len(relaxed) != n:
+            weight, u = heapq.heappop(pq)
+            if u in relaxed:
+                continue
+            relaxed.add((u))
+
+            for new_weight, v in g[u]:
+                if distance[v - 1] == -1 or new_weight + weight < distance[v - 1]:
+                    distance[v - 1] = new_weight + weight
+                heapq.heappush(pq, (distance[v - 1], v))
+
+        ans = distance[0]
+        for num in distance:
+            if num == -1:
+                return -1
+            else:
+                ans = max(num, ans)
+
+        return ans
