e8846ba146ebcc0cdbeb40effa08ba26e8ed471d
PeterWuCs <157049093+PeterWuCs@users.noreply.github.com>
2026-07-07 16:07:52 -0400

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        memo = [-1] * (amount + 1)
+        memo[0] = 0
+    
+        for i in range(1, amount + 1):
+            for coin in coins:
+                subproblem = i - coin
+                print(subproblem)
+                if subproblem < 0:
+                    continue
+
+                if memo[subproblem] != -1:
+                    if memo[i] == -1:
+                        memo[i] = memo[subproblem] + 1
+                    else:
+                        memo[i] = min(memo[subproblem] + 1, memo[i])
+                     
+                    
+        print(memo)
+        return memo[-1]
